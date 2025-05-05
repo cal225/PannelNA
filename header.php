0356bc45de862dabd2234ccaba8d2fba7d44f8c0
banane <calixte.marbach@oumpahpah.fr>
2025-05-05 14:47:41 +0200

    cache control removed

--- header.php
+++ header.php
@@ -6,9 +6,9 @@ echo "
 <head>
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <meta http-equiv='Cache-Control' content='no-cache, no-store, must-revalidate'>
-<meta http-equiv='Pragma' content='no-cache'>
-<meta http-equiv='Expires' content='0'>
+    // <meta http-equiv='Cache-Control' content='no-cache, no-store, must-revalidate'>
+    // <meta http-equiv='Pragma' content='no-cache'>
+    // <meta http-equiv='Expires' content='0'>
     <link rel='stylesheet' href='style/style.css'>
     <link rel='stylesheet' href='style/{$title}.css'>
     <title>{$title}</title>
